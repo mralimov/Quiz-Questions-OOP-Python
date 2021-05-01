ea4d5576cb26e104816c0afad10e78468ab9943e
mralimov <mralimov89@gmail.com>
2021-05-01 16:14:46 -0400

    end of the quiz functionality

--- quiz_ui.py
+++ quiz_ui.py
@@ -35,8 +35,16 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text, text="You are in end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_choice(self):
         self.give_feedback(self.quiz.check_answer("True"))

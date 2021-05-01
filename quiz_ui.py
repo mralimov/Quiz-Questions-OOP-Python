ab2ff3885a0c492fcb7551ac27264fe2c70f8b59
mralimov <mralimov89@gmail.com>
2021-05-01 14:36:02 -0400

    setup UI of quizz

---
+++ quiz_ui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from quiz_model import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.window.mainloop()

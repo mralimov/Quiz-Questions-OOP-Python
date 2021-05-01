7df8c63441273b0a3969eb7cbde335b81d8a0b73
mralimov <mralimov89@gmail.com>
2021-05-01 14:40:16 -0400

    Next Question function

--- quiz_ui.py
+++ quiz_ui.py
@@ -33,3 +33,7 @@ class QuizInterface:
         self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
